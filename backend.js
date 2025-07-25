933c461044ed6d5583d42ca736d53693ecf4ecdf
Meenakshi-19cse <meenakshiharish2006@gmail.com>
2025-07-25 14:53:55 +0530

    backend.js in backend

---
+++ backend.js
@@ -0,0 +1,17 @@
+const attendanceList=[];
+
+function markAttendance(name){
+    attendanceList.push(name);
+    console.log(`Student ${name} is marked present`);
+}
+
+document.getElementById('attendanceForm').addEventListener('submit',function(event){
+  event.preventDefault();
+const studentName = this.student.value.trim();
+if(studentName){
+ markAttendance(studentName);
+ alert(`${studentName} attendance marked!`);
+ this.reset();
+}
+});
+
